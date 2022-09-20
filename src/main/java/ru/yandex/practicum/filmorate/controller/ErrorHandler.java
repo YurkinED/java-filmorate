7486b68d35fad85ac210a1164c43c16e1ee6a539
Solntsev Maksim <solntsev1099@gmail.com>
2022-09-20 21:59:01 +0300

    Версия 4. <feat> Добавлен класс ErrorHandler для обработки исключений.           <feat> Добавлен класс FilmIdCounter для присвоения id фильмам.           <feat> Добавлен класс ErrorResponse, создающий шаблон ответа по ошибке.           <feat> Добавлены классы FilmService и UserService, реализующие бизнес-логику приложения.           <feat> Добавлены классы InMemoryFilmStorage и InMemoryUserStorage реализующие процесс хранения объектов.           <feat> Добавлены интерфейсы FilmStorage и UserStorage.           <refactor> Адаптированы тестовые классы FilmControllerTest и UserControllerTest.

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -29,11 +30,17 @@ public class ErrorHandler {
 
     @ExceptionHandler({InvalidNameException.class, LongDescriptionException.class, NegativeDurationException.class,
             ReleaseDateException.class, InvalidBirthDateException.class, InvalidEmailException.class,
-            InvalidLoginException.class, UserAlreadyExistException.class})
+            InvalidLoginException.class, UserAlreadyExistException.class, MethodArgumentNotValidException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidateParameterException(final RuntimeException e) {
         return new ErrorResponse(e.getMessage());
     }
+
+   /* @ExceptionHandler({InvalidNameException.class, LongDescriptionException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidateParameterException(final RuntimeException e) {
+        return new ErrorResponse(e.getMessage());
+    }*/
 
     @ExceptionHandler({LikesException.class, InvalidIdException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -5,11 +5,11 @@ import net.bytebuddy.utility.RandomString;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import ru.yandex.practicum.filmorate.exceptions.InvalidIdException;
 import ru.yandex.practicum.filmorate.exceptions.filmExceptions.*;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -17,13 +17,13 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.time.LocalDate;
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class FilmControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -82,7 +82,7 @@ public class FilmControllerTest {
                 )
                 .andExpect(status().isBadRequest())
                 .andExpect(result ->
-                        assertTrue(result.getResolvedException() instanceof MethodArgumentNotValidException));
+                        assertTrue(result.getResolvedException() instanceof InvalidNameException));
     }
 
     @Test
@@ -96,7 +96,7 @@ public class FilmControllerTest {
                 )
                 .andExpect(status().isBadRequest())
                 .andExpect(result
-                        -> assertTrue(result.getResolvedException() instanceof MethodArgumentNotValidException));
+                        -> assertTrue(result.getResolvedException() instanceof LongDescriptionException));
     }
 
 
@@ -111,7 +111,7 @@ public class FilmControllerTest {
                 )
                 .andExpect(status().isBadRequest())
                 .andExpect(result
-                        -> assertTrue(result.getResolvedException() instanceof MethodArgumentNotValidException));
+                        -> assertTrue(result.getResolvedException() instanceof NegativeDurationException));
     }
 
     @Test
@@ -125,7 +125,7 @@ public class FilmControllerTest {
                 )
                 .andExpect(status().isBadRequest())
                 .andExpect(result
-                        -> assertTrue(result.getResolvedException() instanceof MethodArgumentNotValidException));
+                        -> assertTrue(result.getResolvedException() instanceof NegativeDurationException));
     }
 
     @Test
