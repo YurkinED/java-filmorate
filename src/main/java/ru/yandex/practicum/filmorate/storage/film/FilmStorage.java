4b961f307dec990c717c84849745b661950acd6b
mkudryavtsev <mkudryavtsev90@yandex.ru>
2022-11-09 14:25:18 +0600

    fix: fix minor typos

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -4,6 +4,7 @@ import ru.yandex.practicum.filmorate.model.Film;
 
 import javax.validation.Valid;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface FilmStorage {
@@ -14,5 +15,25 @@ public interface FilmStorage {
     Film updateFilm(@Valid Film film);
 
     Optional<Film> findFilmById(int filmId);
+
+    boolean checkLikeFilm(int filmId, int userId);
+
+    void likeFilmOrRemoveLike(int filmId, int userId, boolean flag);
+
+    List<Film> findFilmsByDirectorAndSort(int directorId, String query);
+
+    List<Film> commonLikedFilms(int userId, int friendId);
+
+    void deleteFilmById(int filmId);
+
+    List<Film> showMostLikedFilmsFilter(Integer limit, Integer genreId, String year);
+
+    Collection<Film> getRecommendations(int userId);
+
+    List<Film> searchFilmsByTitle(String query);
+
+    List<Film> searchFilmsByDirector(String query);
+
+    List<Film> searchFilmsByTitleAndDirector(String query);
 
 }
