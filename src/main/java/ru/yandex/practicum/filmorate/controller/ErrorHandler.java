90a670d89475fcebad08db45bd9b887525418a8f
Solntsev Maksim <solntsev1099@gmail.com>
2022-11-09 21:07:31 +0300

    <refactor> В класс ErrorHandler добавлено логирование и хендлер для обработки всех необработанных исключений.

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,18 +15,28 @@ import ru.yandex.practicum.filmorate.exceptions.userExceptions.InvalidLoginExcep
 import ru.yandex.practicum.filmorate.exceptions.userExceptions.UserAlreadyExistException;
 import ru.yandex.practicum.filmorate.model.ErrorResponse;
 
+@Slf4j
 @RestControllerAdvice("ru.yandex.practicum.filmorate.controller")
 public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleSystemExceptions(final IllegalArgumentException e) {
+        log.debug("Упс. Кажется, возникла ошибка {},", e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleSystemExceptions(final NullPointerException e) {
+        log.debug("Упс. Кажется, возникла ошибка {},", e.getMessage());
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleSystemExceptions(final Throwable e) {
+        log.debug("Упс. Кажется, возникла ошибка {},", e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 
@@ -36,12 +47,14 @@ public class ErrorHandler {
             MethodArgumentNotValidException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidateParameterException(final RuntimeException e) {
+        log.debug("Упс. Кажется, возникла ошибка {},", e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler({LikesException.class, InvalidIdException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleSystemExceptions(final RuntimeException e) {
+        log.debug("Упс. Кажется, возникла ошибка {},", e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
@@ -4,6 +4,7 @@ public class ErrorResponse {
     private final String error;
 
     public ErrorResponse(String error) {
+
         this.error = error;
     }
 
